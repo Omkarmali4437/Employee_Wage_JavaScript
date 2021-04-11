115cc3b140559f459334c5c12b6ee0ac00a6eef9
omkarmali4437 <omkarmalli688@gmail.com>
2021-04-11 17:55:17 +0530

    Added logic for calculating total wage for employee for maximum 20days and 100hrs

--- EmpWage.js
+++ EmpWage.js
@@ -4,6 +4,7 @@ const FULLTIME_HRS=8;
 const PARTTIME_HRS=4;
 const WAGE_PER_HR=20;
 const MAX_NUMBER_OF_WORKING_DAYS=20;
+const TOTAL_WORKING_HRS=100;
 
 function getempHrs(empcheck){
     switch(empcheck){
@@ -16,11 +17,14 @@ function getempHrs(empcheck){
     }
 }
 
-let emphrs=0;
-for(let day=0;day<=MAX_NUMBER_OF_WORKING_DAYS;day++){
+let totalWorkinghrs=0;
+let totalworkingDays=0;
+while(totalWorkinghrs<=TOTAL_WORKING_HRS && totalworkingDays<MAX_NUMBER_OF_WORKING_DAYS){
+    totalworkingDays++;
     let empcheck=Math.floor((Math.random()*10)%3);
-    emphrs+=getempHrs(empcheck);
+    totalWorkinghrs+=getempHrs(empcheck);
 }
-let empWage=emphrs*WAGE_PER_HR;
-console.log("Total Hours: "+emphrs);
+let empWage=totalWorkinghrs*WAGE_PER_HR;
+console.log("Total Hours: "+totalWorkinghrs);
+console.log("Total Working Days: "+totalworkingDays);
 console.log("Daily Wage of a employee is: "+empWage);
