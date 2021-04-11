8436d7b16c711f9d398de1b795238a4614a2227b
omkarmali4437 <omkarmalli688@gmail.com>
2021-04-11 17:46:00 +0530

    Added logic for emp wage for 20 days

--- EmpWage.js
+++ EmpWage.js
@@ -3,6 +3,7 @@ const IS_FULLTIME=2;
 const FULLTIME_HRS=8;
 const PARTTIME_HRS=4;
 const WAGE_PER_HR=20;
+const MAX_NUMBER_OF_WORKING_DAYS=20;
 
 function getempHrs(empcheck){
     switch(empcheck){
@@ -16,7 +17,10 @@ function getempHrs(empcheck){
 }
 
 let emphrs=0;
-let empcheck=Math.floor((Math.random()*10)%3);
-emphrs=getempHrs(empcheck);
+for(let day=0;day<=MAX_NUMBER_OF_WORKING_DAYS;day++){
+    let empcheck=Math.floor((Math.random()*10)%3);
+    emphrs+=getempHrs(empcheck);
+}
 let empWage=emphrs*WAGE_PER_HR;
+console.log("Total Hours: "+emphrs);
 console.log("Daily Wage of a employee is: "+empWage);
