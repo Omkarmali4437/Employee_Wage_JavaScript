2a095a92efe61000f81e6d5c91d1711a6fe3c426
omkarmali4437 <omkarmalli688@gmail.com>
2021-04-11 17:37:48 +0530

    Added logic for calculating daily wage of a employee

--- EmpWage.js
+++ EmpWage.js
@@ -1,11 +1,24 @@
-const IS_ABSNET=0;
-const IS_PRESENT=1;
+const IS_PARTTIME=1;
+const IS_FULLTIME=2;
+const FULLTIME_HRS=8;
+const PARTTIME_HRS=4;
+const WAGE_PER_HR=20;
 
-let empcheck=Math.floor((Math.random()*10)%2);
+let emphrs=0;
 
-if(empcheck == IS_ABSNET){
-    console.log("Employee is Absent");
+let empcheck=Math.floor((Math.random()*10)%3);
+
+switch(empcheck){
+    case 1:
+        emphrs=PARTTIME_HRS;
+        break;
+    case 2:
+        emphrs=FULLTIME_HRS;
+        break;
+    default:
+        emphrs=0;
+        break;
 }
-else{
-    console.log("Employee is Present");
-}
+
+let empWage=emphrs*WAGE_PER_HR;
+console.log("Daily Wage of a employee is: "+empWage);
